51fc7495d41127a6480b3e4c51a3c8bcf37b70de
csabe812 <csafulop@gmail.com>
2019-09-28 15:00:42 +0200

    FEATURE | Started to implement backend

---
+++ backend/src/main/java/com/springwebsitewithangular/SpringWebsiteWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.springwebsitewithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsiteWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsiteWithAngularApplication.class, args);
+	}
+
+}
